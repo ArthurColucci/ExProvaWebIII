a464d630f6b7dd4733bbaadd025ba79277cb83a2
ArthurColucci <arthurkoluk@gmail.com>
2023-06-12 20:07:00 -0300

    Adicionar arquivos de projeto.

---
+++ ExProvaWebIII/Controllers/ProductsController.cs
@@ -0,0 +1,68 @@
+﻿using ExProvaWebIII.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExProvaWebIII.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly List<Product> _products = new List<Product>(); // Simulação de armazenamento em memória
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return _products;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> Get(int id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+
+        [HttpPost]
+        public ActionResult<Product> Create(Product product)
+        {
+            product.Id = _products.Count + 1;
+            _products.Add(product);
+            return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Product updatedProduct)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            product.Nome = updatedProduct.Nome;
+            product.Valor = updatedProduct.Valor;
+            product.Quantidade = updatedProduct.Quantidade;
+            product.Descricao = updatedProduct.Descricao;
+            product.CodigoBarras = updatedProduct.CodigoBarras;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _products.Remove(product);
+            return NoContent();
+        }
+    }
+
+}
